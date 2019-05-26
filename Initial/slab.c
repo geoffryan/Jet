ce123c27222acf8af039ebc7d1bfd86c30e2650c
Geoffrey Ryan <geoffrey.s.ryan@gmail.com>
2019-05-26 15:24:48 -0400

    slab setup uses more in.par stuff

--- Initial/slab.c
+++ Initial/slab.c
@@ -4,11 +4,15 @@
 static double T_MIN = 0.0; 
 static double E0 = 0.0;
 static double g0 = 0.0;
+static double th_j = 0.0;
+static double dR = 0.0;
 
 void setICparams( struct domain * theDomain ){
    T_MIN = theDomain->theParList.t_min;
    E0 = theDomain->theParList.Explosion_Energy;
    g0 = theDomain->theParList.Gam_0;
+   th_j = theDomain->theParList.Nozzle_th0;
+   dR = theDomain->theParList.Nozzle_r0;
 }
 
 void initial( double * prim , double * x ){
@@ -17,20 +21,17 @@ void initial( double * prim , double * x ){
    double th = x[1];
 
    double t   = T_MIN;
-   double th_j = M_PI;
-   double delta = 0.03;
 
    double rho0 = 1.0;
    double cs20 = 1.0e-6;
 
    double Rf = t*sqrt(1.0 - 1.0/(g0*g0));
-   double Rb = (1-delta)*Rf;
+   double Rb = Rf-dR;
 
    double rho, u, Pp, X;
 
    if( r < Rf && r > Rb && th < th_j)
    {
-       double dR = Rf - Rb;
        double V = 4*M_PI/3.0 * (Rf*Rf+Rf*Rb+Rb*Rb) * dR;
        double M = E0 / (g0 - 1.0);
        rho = M / (g0*V);
